faf747a5182482ebd65d9f048f3da596a5563285
UsuarioN°1000 <UsuarioN1000@192.168.1.86>
2018-06-08 12:08:49 -0400

    1 y 2 orden

---
+++ src/instruccionesprogramar/InstruccionesProgramar.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package instruccionesprogramar;
+
+/**
+ *
+ * @author UsuarioN°1000
+ */
+public class InstruccionesProgramar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("1 debe tener una idea");
+        System.out.println("2 debe escribir esa idea");
+    }
+    
+}
