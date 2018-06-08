ceb672b153ac202fb515266dcf403af876b91fe1
UsuarioN°1000 <UsuarioN1000@192.168.1.86>
2018-06-08 12:16:12 -0400

    3 orden y 2 clase de instrucciones

---
+++ src/instruccionesprogramar/InstruccionesProgramar2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package instruccionesprogramar;
+
+/**
+ *
+ * @author UsuarioN°1000
+ */
+public class InstruccionesProgramar2 {
+
+    public InstruccionesProgramar2() {
+        System.out.println("Instrucciones 2");
+    }
+    
+}
